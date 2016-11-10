5ff819ff0c81f2e5c8bcd289bdd3df2e1ec90995
Jerome <jdc@free.fr>
2016-11-10 12:35:04 +0100

    Working rough version

---
+++ iOS_NIM/NIMGame.swift
@@ -0,0 +1,70 @@
+//
+//  NIMGame.swift
+//  iOS_NIM
+//
+//  Created by imac on 10/11/2016.
+//  Copyright © 2016 imac. All rights reserved.
+//
+
+import Foundation
+
+class NIMGame {
+    public static let maxMatches:Int = 20
+    private static let maxRemovableMatches:Int = 3
+    
+    private var humanVsHuman:Bool = false
+    public var remainingMatches:Int = maxMatches
+    public var currentPlayer:Int = 1
+    public var maxInput:Int {
+        get{
+            return min(remainingMatches, NIMGame.maxRemovableMatches)
+        }
+    }
+    init(humanVsHuman:Bool) {
+        reset(hVsH: humanVsHuman)
+    }
+    func newGame(humanVsHuman:Bool) {
+        reset(hVsH: humanVsHuman)
+    }
+    func play(nbMatchesSelected:Int) {
+        if (currentPlayer == 2 && !humanVsHuman) {
+            playIA()
+        } else {
+            removeMatches(nbMatches: nbMatchesSelected)
+        }
+    }
+    func isGameOver() -> Bool {
+        return remainingMatches == 0
+    }
+    private func playIA() {
+        let nbMatchesSelected:Int
+        if (remainingMatches % (NIMGame.maxRemovableMatches + 1) != 1) {
+            nbMatchesSelected = ((remainingMatches - 1) % (NIMGame.maxRemovableMatches + 1))
+        } else {
+            nbMatchesSelected = generateRandomNumber(min:1, max:maxInput)
+        }
+        removeMatches(nbMatches: nbMatchesSelected)
+    }
+    func reset(hVsH:Bool) {
+        humanVsHuman = hVsH
+        remainingMatches = 20
+        currentPlayer = generateRandomNumber(min:1, max:2)
+        if (!hVsH && currentPlayer == 2) {
+            playIA()
+        }
+    }
+    private func removeMatches(nbMatches:Int) {
+        remainingMatches = remainingMatches - nbMatches
+        if (remainingMatches > 0) {
+            if (currentPlayer == 1) {
+                currentPlayer = 2
+            } else {
+                currentPlayer = 1
+            }
+        }
+    }
+    private func generateRandomNumber(min:Int, max:Int) -> Int {
+        let range = max - min + 1
+        return Int(arc4random_uniform(UInt32(range))) + min
+    }
+}
--- iOS_NIM/ViewController.swift
+++ iOS_NIM/ViewController.swift
@@ -9,11 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    private var myNIMGame:NIMGame?
     @IBOutlet weak var labelIndicator: UILabel!
     @IBOutlet weak var chooseMatch1: UIImageView!
     @IBOutlet weak var chooseMatch2: UIImageView!
     @IBOutlet weak var chooseMatch3: UIImageView!
+    @IBOutlet var remainingMatches: [UIImageView]!
+    @IBOutlet weak var sliderNbMatches: UISlider!
+    @IBOutlet weak var choiceHumanVsHuman: UISwitch!
+    @IBAction func choiceChangeHumanVsHuman(_ sender: UISwitch) {
+        let alert = UIAlertController(title: "Change player 2", message: "If you change this, you will stop your current game. Do you really want to do it?", preferredStyle: UIAlertControllerStyle.alert)
+        self.present(alert, animated: true, completion: nil)
+        alert.addAction(UIAlertAction(title: "No", style: UIAlertActionStyle.default, handler: {
+            action in
+            switch action.style{
+            default:
+                if (sender.isOn) {
+                    sender.isOn = false
+                } else {
+                    sender.isOn = true
+                }
+            }
+        }))
+        alert.addAction(UIAlertAction(title: "Yes", style: UIAlertActionStyle.default, handler: {
+            action in
+            switch action.style{
+            default:
+                self.restartGame()
+            }
+        }))
+    }
     @IBAction func sliderChangeNbMatches(_ sender: UISlider) {
         let roundedValue = round(sender.value)
         sender.value = roundedValue
@@ -31,23 +56,56 @@ class ViewController: UIViewController {
             chooseMatch2.isHidden = false
             chooseMatch3.isHidden = false
         default:
-            chooseMatch1.isHidden = false
-            chooseMatch2.isHidden = false
-            chooseMatch3.isHidden = false
+            chooseMatch1.isHidden = true
+            chooseMatch2.isHidden = true
+            chooseMatch3.isHidden = true
         }
     }
     @IBAction func btnPlay(_ sender: UIButton) {
+        myNIMGame?.play(nbMatchesSelected: Int(sliderNbMatches.value))
+        setDisplay()
     }
     override func viewDidLoad() {
         super.viewDidLoad()
+        myNIMGame = NIMGame(humanVsHuman:isHumanVsHuman())
+        setDisplay()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    private func setDisplay() {
+        labelIndicator.text = "Player \((myNIMGame?.currentPlayer)!) - Remaining \((myNIMGame?.remainingMatches)!) matches"
+        for i:Int in 0..<NIMGame.maxMatches {
+            remainingMatches[i].isHidden = (i >= (myNIMGame?.remainingMatches)!)
+        }
+        sliderNbMatches.maximumValue = Float((myNIMGame?.maxInput)!)
+        if (sliderNbMatches.maximumValue > 0) {
+            sliderNbMatches.minimumValue = 1
+        }
+        sliderChangeNbMatches(sliderNbMatches)
+        if (myNIMGame?.isGameOver())! {
+            gameOver()
+        }
+    }
+    func gameOver() {
+        let alert = UIAlertController(title: "Game over", message: "Player \((myNIMGame?.currentPlayer)!) has lost!", preferredStyle: UIAlertControllerStyle.alert)
+        self.present(alert, animated: true, completion: nil)
+        alert.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.default, handler: {
+            action in
+            switch action.style{
+            default:
+                self.restartGame()
+            }
+        }))
+    }
+    private func isHumanVsHuman() -> Bool {
+        return choiceHumanVsHuman.isOn
+    }
+    private func restartGame() {
+        myNIMGame?.newGame(humanVsHuman:isHumanVsHuman())
+        setDisplay()
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
